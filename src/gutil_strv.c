70a5d3fdcb0cae0739d30178e488a3528f5c1713
Slava Monich <slava.monich@jolla.com>
2019-02-13 19:53:12 +0300

    [glibutil] Added gutil_strv_last() function

--- src/gutil_strv.c
+++ src/gutil_strv.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2014-2017 Jolla Ltd.
- * Contact: Slava Monich <slava.monich@jolla.com>
+ * Copyright (C) 2014-2019 Jolla Ltd.
+ * Copyright (C) 2014-2019 Slava Monich <slava.monich@jolla.com>
  *
  * You may use this file under the terms of BSD license as follows:
  *
@@ -13,9 +13,9 @@
  *   2. Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- *   3. Neither the name of Jolla Ltd nor the names of its contributors may
- *      be used to endorse or promote products derived from this software
- *      without specific prior written permission.
+ *   3. Neither the names of the copyright holders nor the names of its
+ *      contributors may be used to endorse or promote products derived
+ *      from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -65,6 +65,21 @@ gutil_strv_at(
             /* We also end up here if i == len but that's OK */
             return sv[k];
         }
+    }
+    return NULL;
+}
+
+/**
+ * Returns the last string in the array, NULL if there's nothing there.
+ */
+const char*
+gutil_strv_last(
+    const GStrV* sv) /* Since 1.0.35 */
+{
+    if (G_LIKELY(sv) && G_LIKELY(sv[0])) {
+        guint k = 0;
+        while (sv[k + 1]) k++;
+        return sv[k];
     }
     return NULL;
 }
