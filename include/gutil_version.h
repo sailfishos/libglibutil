2e561fa6e8e6aa9e19813c6c02cd64b0e0b3017a
Slava Monich <slava@monich.com>
2023-06-18 14:41:54 +0300

    Version 1.0.71
    [glibutil] JB#33786

--- include/gutil_version.h
+++ include/gutil_version.h
@@ -46,8 +46,8 @@ G_BEGIN_DECLS
 
 #define GUTIL_VERSION_MAJOR   1
 #define GUTIL_VERSION_MINOR   0
-#define GUTIL_VERSION_MICRO   70
-#define GUTIL_VERSION_STRING  "1.0.70"
+#define GUTIL_VERSION_MICRO   71
+#define GUTIL_VERSION_STRING  "1.0.71"
 
 extern const guint gutil_version_major; /* GUTIL_VERSION_MAJOR */
 extern const guint gutil_version_minor; /* GUTIL_VERSION_MINOR */
@@ -74,6 +74,7 @@ extern const guint gutil_version_micro; /* GUTIL_VERSION_MICRO */
 /* Specific versions */
 #define GUTIL_VERSION_1_0_69 GUTIL_VERSION_(1,0,69)
 #define GUTIL_VERSION_1_0_70 GUTIL_VERSION_(1,0,70)
+#define GUTIL_VERSION_1_0_71 GUTIL_VERSION_(1,0,71)
 
 G_END_DECLS
 
